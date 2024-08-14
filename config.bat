7dfb3c78411edf6b7734a00171519fb9544471f2
aaroned <aaroned@users.noreply.github.com>
2024-08-14 20:25:58 +1000

    update config script to add firewall rules

--- config.bat
+++ config.bat
@@ -1,3 +1,4 @@
 @echo off
 netsh http add urlacl url=http://+:7004/ user=\Everyone
-pause
+netsh advfirewall firewall add rule name="KH Switcher" dir=in action=allow protocol=TCP localport=7004 profile=private remoteip=localsubnet
+netsh advfirewall firewall add rule name="KH Switcher" dir=in action=allow protocol=TCP localport=7004 profile=public remoteip=localsubnet
