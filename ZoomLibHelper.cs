ea131405a290e470b2bc1d0c85364207d14e753a
yarko-w <165334719+yarko-w@users.noreply.github.com>
2026-02-14 16:28:36 +1200

    Fix for Zoom
    Zoom changed it's window caption name to "Zoom Meeting". Unfortunately this is also the name of the main Zoom window usually on the AV desktop monitors and there was no way to differentiate between the too. This fix adds a method of looking for Zoom windows to interact with on the target display (Second display which is usually the halls TV's).

--- ZoomLibHelper.cs
+++ ZoomLibHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;   
 using System.Text;
 
 namespace JW_Library_Focuser
@@ -9,10 +10,12 @@ namespace JW_Library_Focuser
     {
         private const string ZoomLibProcessName = "Zoom";
         private const string MainWindowClassName = "ConfMultiTabContentWndClass";
-        private const string ZoomLibCaptionPrefix = "Zoom Workplace";
+        private const string ZoomLibCaptionPrefix = "Zoom Meeting";
 
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        // Specify the monitor index to bring Zoom to front. (0 = primary, 1 = secondary, ect.)
+        private static int TargetMonitorIndex = 1;
         public static void BringToFront()
         {
             if (log.IsInfoEnabled) log.Info("Attempting to bring Zoom to front");
@@ -43,6 +46,14 @@ namespace JW_Library_Focuser
                     return false;
                 }
 
+                // Get target monitor first
+                IntPtr targetMonitor = GetMonitorByIndex(TargetMonitorIndex);
+                if (targetMonitor == IntPtr.Zero)
+                {
+                    if (log.IsInfoEnabled) log.Info($"Cannot find monitor at index {TargetMonitorIndex}");
+                    return false;
+                }
+
                 bool found = false;
                 var prevWindow = IntPtr.Zero;
 
@@ -56,13 +67,24 @@ namespace JW_Library_Focuser
 
                     var sb = new StringBuilder(256);
                     LibHelperNativeMethods.GetWindowText(mainWindow, sb, 256);
-                    log.Info(sb.ToString());
+                    log.Info($"Found window: {sb}");
+
                     if (sb.ToString().Equals(ZoomLibCaptionPrefix))
                     {
-                        LibHelperNativeMethods.ShowWindow(mainWindow, LibHelperNativeMethods.SW_MAXIMIZE);
-                        LibHelperNativeMethods.SetForegroundWindow(mainWindow);
-                        if (log.IsInfoEnabled) log.Info($"{processName} window brought to foreground.");
-                        found = true;
+                        // Check if this window is on the target monitor
+                        IntPtr windowMonitor = LibHelperNativeMethods.MonitorFromWindow(mainWindow, LibHelperNativeMethods.MONITOR_DEFAULTTONEAREST);
+
+                        if (windowMonitor == targetMonitor)
+                        {
+                            LibHelperNativeMethods.ShowWindow(mainWindow, LibHelperNativeMethods.SW_MAXIMIZE);
+                            LibHelperNativeMethods.SetForegroundWindow(mainWindow);
+                            if (log.IsInfoEnabled) log.Info($"{processName} window on monitor {TargetMonitorIndex} brought to foreground.");
+                            found = true;
+                        }
+                        else
+                        {
+                            if (log.IsInfoEnabled) log.Info($"Window found but on different monitor, continuing search...");
+                        }
                     }
 
                     prevWindow = mainWindow;
@@ -100,6 +122,14 @@ namespace JW_Library_Focuser
                     return false;
                 }
 
+                // Get target monitor first
+                IntPtr targetMonitor = GetMonitorByIndex(TargetMonitorIndex);
+                if (targetMonitor == IntPtr.Zero)
+                {
+                    if (log.IsInfoEnabled) log.Info($"Cannot find monitor at index {TargetMonitorIndex}");
+                    return false;
+                }
+
                 bool found = false;
                 var prevWindow = IntPtr.Zero;
 
@@ -113,11 +143,22 @@ namespace JW_Library_Focuser
 
                     var sb = new StringBuilder(256);
                     LibHelperNativeMethods.GetWindowText(mainWindow, sb, 256);
+
                     if (sb.ToString().Equals(ZoomLibCaptionPrefix))
                     {
-                        LibHelperNativeMethods.ShowWindow(mainWindow, LibHelperNativeMethods.SW_MINIMIZE);
-                        if (log.IsInfoEnabled) log.Info($"{processName} window minimized.");
-                        found = true;
+                        // Check if this window is on the target monitor
+                        IntPtr windowMonitor = LibHelperNativeMethods.MonitorFromWindow(mainWindow, LibHelperNativeMethods.MONITOR_DEFAULTTONEAREST);
+
+                        if (windowMonitor == targetMonitor)
+                        {
+                            LibHelperNativeMethods.ShowWindow(mainWindow, LibHelperNativeMethods.SW_MINIMIZE);
+                            if (log.IsInfoEnabled) log.Info($"{processName} window on monitor {TargetMonitorIndex} minimized.");
+                            found = true;
+                        }
+                        else
+                        {
+                            if (log.IsInfoEnabled) log.Info($"Window found but on different monitor, continuing search...");
+                        }
                     }
 
                     prevWindow = mainWindow;
@@ -135,6 +176,24 @@ namespace JW_Library_Focuser
                 log.Error(exc.Message, exc);
                 throw;
             }
+        }
+        private static IntPtr GetMonitorByIndex(int index)
+        {
+            var monitors = new System.Collections.Generic.List<IntPtr>();
+
+            LibHelperNativeMethods.EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
+                delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref LibHelperNativeMethods.RECT lprcMonitor, IntPtr dwData)
+                {
+                    monitors.Add(hMonitor);
+                    return true;
+                }, IntPtr.Zero);
+
+            if (index >= 0 && index < monitors.Count)
+            {
+                return monitors[index];
+            }
+
+            return IntPtr.Zero;
         }
     }
 }
